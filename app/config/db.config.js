f42d4200c6bbb1f6284838c819006c7b1abc8521
Shiladitya Bose <shiladitya_bose@persistent.com>
2023-12-26 18:01:24 +0530

    Added database credentials

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,9 @@
-require('dotenv').config()
+// require('dotenv').config()
 module.exports = {
-    HOST: process.env.HOST || 'ec2-18-211-215-8.compute-1.amazonaws.com',
-    USER: process.env.USER || 'egabsinpheuafx',
-    PASSWORD: process.env.PASSWORD || 'ee3a02723eccb01e971556e5efbf3e8fcf9094adbb12915e9c9a091fc62a0151',
-    DB: process.env.DB || 'de2gkpp43b84i9',
+    HOST: "ec2-18-211-215-8.compute-1.amazonaws.com",
+    USER:  "egabsinpheuafx",
+    PASSWORD: "ee3a02723eccb01e971556e5efbf3e8fcf9094adbb12915e9c9a091fc62a0151",
+    DB: "de2gkpp43b84i9",
     dialect: "postgres",
     pool: {
         max:5,
